5a1838dc2ed15860f2e49caa9db53280c63ecc93
Nilupul Udara <120694681+NilupulUdara@users.noreply.github.com>
2025-10-15 10:22:03 +0530

    seed data for account type

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -25,6 +25,7 @@ class DatabaseSeeder extends Seeder
             ChartClassSeeder::class, 
             ChartTypesSeeder::class,
             ChartMasterSeeder::class, 
+            AccountTypeSeeder::class,
         ]);
     }
 }
