aea47c7ff1a1cbfa6f29409d0b87d7278ad5a7f6
Nilupul Udara <120694681+NilupulUdara@users.noreply.github.com>
2025-12-30 17:06:54 +0530

    updated debtor trans request

--- backend/app/Http/Requests/DebtorTransRequest.php
+++ backend/app/Http/Requests/DebtorTransRequest.php
@@ -21,30 +21,62 @@ class DebtorTransRequest extends FormRequest
      */
     public function rules(): array
     {
-        return [
-            'trans_no' => 'required|integer',
-            'trans_type' => 'required|integer',
-            'version' => 'nullable|integer',
-            'debtor_no' => 'required|integer',
-            'branch_code' => 'required|integer',
-            'tran_date' => 'required|date',
-            'due_date' => 'required|date',
-            'reference' => 'nullable|string|max:60',
-            'tpe' => 'nullable|integer',
-            'order_no' => 'nullable|integer',
-            'ov_amount' => 'nullable|numeric',
-            'ov_gst' => 'nullable|numeric',
-            'ov_freight' => 'nullable|numeric',
-            'ov_freight_tax' => 'nullable|numeric',
-            'ov_discount' => 'nullable|numeric',
-            'alloc' => 'nullable|numeric',
-            'prep_amount' => 'nullable|numeric',
-            'rate' => 'nullable|numeric',
-            'ship_via' => 'nullable|integer',
-            'dimension_id' => 'nullable|integer',
-            'dimension2_id' => 'nullable|integer',
-            'payment_terms' => 'nullable|integer',
-            'tax_included' => 'nullable|boolean',
-        ];
+        if ($this->isMethod('post')) {
+            return [
+                'trans_no' => 'required|integer',
+                'trans_type' => 'required|integer',
+                'version' => 'nullable|integer',
+                'debtor_no' => 'required|integer',
+                'branch_code' => 'required|integer',
+                'tran_date' => 'required|date',
+                'due_date' => 'required|date',
+                'reference' => 'nullable|string|max:60',
+                'tpe' => 'nullable|integer',
+                'order_no' => 'nullable|integer',
+                'ov_amount' => 'nullable|numeric',
+                'ov_gst' => 'nullable|numeric',
+                'ov_freight' => 'nullable|numeric',
+                'ov_freight_tax' => 'nullable|numeric',
+                'ov_discount' => 'nullable|numeric',
+                'alloc' => 'nullable|numeric',
+                'prep_amount' => 'nullable|numeric',
+                'rate' => 'nullable|numeric',
+                'ship_via' => 'nullable|integer',
+                'dimension_id' => 'nullable|integer',
+                'dimension2_id' => 'nullable|integer',
+                'payment_terms' => 'nullable|integer',
+                'tax_included' => 'nullable|boolean',
+            ];
+        }
+
+        if ($this->isMethod('put') || $this->isMethod('patch')) {
+            return [
+                'trans_no' => 'integer',
+                'trans_type' => 'integer',
+                'version' => 'nullable|integer',
+                'debtor_no' => 'integer',
+                'branch_code' => 'integer',
+                'tran_date' => 'date',
+                'due_date' => 'date',
+                'reference' => 'nullable|string|max:60',
+                'tpe' => 'nullable|integer',
+                'order_no' => 'nullable|integer',
+                'ov_amount' => 'nullable|numeric',
+                'ov_gst' => 'nullable|numeric',
+                'ov_freight' => 'nullable|numeric',
+                'ov_freight_tax' => 'nullable|numeric',
+                'ov_discount' => 'nullable|numeric',
+                'alloc' => 'nullable|numeric',
+                'prep_amount' => 'nullable|numeric',
+                'rate' => 'nullable|numeric',
+                'ship_via' => 'nullable|integer',
+                'dimension_id' => 'nullable|integer',
+                'dimension2_id' => 'nullable|integer',
+                'payment_terms' => 'nullable|integer',
+                'tax_included' => 'nullable|boolean',
+            ];
+        }
+
+        return [];
     }
 }
